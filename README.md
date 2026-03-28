3b0335ad54ca4f98b572a49e6df0af6f5fd09565
Soham-Coder-OG <rick4u2003@gmail.com>
2026-03-29 02:31:58 +0530

    added readme file content

---
+++ README.md
@@ -0,0 +1,4 @@
+inside the terminal type :- npm install 
+create one uploads folder in the directory 
+make sure to enter your keys and uri in the .env file 
+to run the server type node server.js 
--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log('📈 MongoDB Connected'))
   .catch(err => console.log(err));
 
 const memorySchema = new mongoose.Schema({
